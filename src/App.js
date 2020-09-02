e9d95abfb1e55faaed56a40571f67f433edd96d1
Kay <joe_kayu@yahoo.com>
2020-09-02 22:02:49 +0100

    .

--- src/App.js
+++ src/App.js
@@ -16,15 +16,11 @@ class App extends Component {
       <React.Fragment>
         <Navbar/>
         <Switch>
-          <Route path="/" component={ProductList} />
+          <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
         </Switch>
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default/>
       </React.Fragment>
     );
   }
