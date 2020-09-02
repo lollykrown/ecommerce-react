da996b4eaa014095b51279a1c070db9bc7d70765
Kay <joe_kayu@yahoo.com>
2020-09-02 21:45:34 +0100

    .

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class Navbar extends Component {
+  
+  render(){
+    return (
+      <div>
+        <h3>Hello from navbar</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  
+  render(){
+    return (
+      <React.Fragment>
+        <h3>Hello from app</h3>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ProductList;
