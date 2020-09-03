958594f602f3c9d80a62d15e56242db6a4cb0352
Kay <joe_kayu@yahoo.com>
2020-09-03 17:56:05 +0100

    .

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,8 +21,15 @@ class Product extends Component {
                   {inCart?(<p className="text-capitalize mb-0" disabled>
                     {""} in Cart</p>):
                   (<i className="fas fa-cart-plus" />)}
-                  
+
                 </button>
+              </div>
+              <div className="card-footer d-flex justify-content-between">
+                <p className="align-self-center mb-0">{title}</p>
+                <h5 className="text-blue font-italic mb-0">
+                  <span className="mr-1">$</span>
+                  {price}
+                </h5>
               </div>
             </div>
         </ProductWrapper>
@@ -31,22 +38,41 @@ class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
+.card{
+  border-color:transparent;
+  transition: all 1s linear;
+}
+.card-footer {
+  background:transparent;
+  border-top: transparent;
+  transition: all 1s linear;
+}
+&:hover{
+  .card{
+    border:0.04rem solid rgba(0,0,0,0.2);
+    box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+  }
+  .card-footer {
+    background:rgba(247,247,247);
+  }
+}
+.img-container {
+  position:relative;
+  overflow:hidden
+}
+.img-container:hover .card-img-top {
+  transform:scale(1,2)
+}
+.card-img-top {
+  transform:scale(1,2)
+}
     // text-transform:capitalize;
     // font-size:1.4rem;
-    // background:transparent;
-    // border:0.125rem solid var(--lightBlue);
     // color:var(--lightBlue);
     // border-radius: 0.5rem;
     // padding: 0.2rems 0.5rem
     // cursor: pointer;
     // margin: 0.2rem 0.5rem 0.2rem 0;
-    // transition: all 0.5s ease-in-out;
-    // &:hover{
-    // background: var(--lightBlue);
-    // color: var(--mainBlue);
-    // }
-    // &:focus{
-    // outline:none;
-    // }
+
 `;
 export default Product;
