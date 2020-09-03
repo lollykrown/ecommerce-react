25ddce742e351a30b643d0171b6c1afab44a4eab
Kay <joe_kayu@yahoo.com>
2020-09-03 16:13:23 +0100

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,22 +35,11 @@ class Navbar extends Component {
 }
 
 const NavWrapper = styled.nav`
-    text-transform:capitalize;
-    font-size:1.4rem;
-    background:transparent;
-    border:0.125rem solid var(--lightBlue);
-    color:var(--lightBlue);
-    border-radius: 0.5rem;
-    padding: 0.2rems 0.5rem
-    cursor: pointer;
-    margin: 0.2rem 0.5rem 0.2rem 0;
-    transition: all 0.5s ease-in-out;
-    &:hover{
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-    }
-    &:focus{
-    outline:none;
+    background:var(--mainBlue);
+    .nav-link{
+      color: var(--mainWhite);
+      font-size:1.3rem;
+      text-transform:capitalize;
     }
 `;
 export default Navbar;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import Title from './Title';
 
 class ProductList extends Component {
-  
+  state = {
+    products: []
+  };
   render(){
     return (
-        <div>
-        <Product></Product>
-      </div>
+        <React.Fragment>
+          <div className="py-5">
+            <div className="container">
+              <Title name="our" title="products" />
+              <div className="row"></div>
+            </div>
+
+          </div>
+        </React.Fragment>
     );
   }
 }
