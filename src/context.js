5f0ac1f3c7e5fd513a92cfff42c866763f4bbb6d
Kay <joe_kayu@yahoo.com>
2020-09-04 12:01:33 +0100

    .

--- src/context.js
+++ src/context.js
@@ -7,6 +7,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct,
+    cart: []
   };
   componentDidMount(){
     this.setProducts();
@@ -22,12 +23,27 @@ class ProductProvider extends Component {
       return { products: tempProducts}
     })
   }
-  handleDetail = () => {
-    console.log("hello from detail");
+
+  getItem = (id) => {
+    const product = this.state.products.find(item=>item.id === id);
+    return product;
+  }
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(()=> {
+      return {detailProduct: product}
+    })
   };
 
   addTocart = (id) => {
-    console.log("hello from add to cart", id);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price
+    product.total = price
   };
 
   render() {
