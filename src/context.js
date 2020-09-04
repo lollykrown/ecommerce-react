94e1a5ee2494e7db6d105ef09a31e3257c269e68
Kay <joe_kayu@yahoo.com>
2020-09-04 20:25:23 +0100

    .

--- src/context.js
+++ src/context.js
@@ -72,19 +72,76 @@ class ProductProvider extends Component {
   }
 
   increment = id => {
-    console.log('increment method')
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count += 1;
+    product.total = product.count * product.price
+
+    this.setState(()=> {
+      return {
+        cart:[...tempCart],
+      }
+    },
+    ()=>{
+      this.addTotals()
+    })
+
   }
 
   decrement = id => {
-    console.log('decrement method')
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count -= 1;
+
+    if(product.count === 0){
+      this.removeItem(id)
+    }else {
+      product.total = product.count * product.price;
+    this.setState(()=> {
+      return {
+        cart:[...tempCart],
+      }
+    },
+    ()=>{
+      this.addTotals()
+    })
+  }
   }
 
   removeItem = id => {
-    console.log('remove item method')
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => item.id !== id)
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index]
+    removedProduct.inCart = false
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(()=> {
+      return {
+        cart:[...tempCart],
+        products: [...tempProducts]
+      }
+    },
+    ()=>{
+      this.addTotals()
+    })
   }
 
   clearCart = () => {
-    console.log('clear cart method')
+    this.setState(()=> {
+      return {cart:[]}
+    },
+    ()=> {
+      this.setProducts();
+      this.addTotals()
+    })
   }
 
   addTotals = () => {
@@ -98,7 +155,6 @@ class ProductProvider extends Component {
         cartSubtotal: subTotal,
         cartTax: tax,
         cartTotal:total
-
       }
     })
   }
