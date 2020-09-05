62a1c47d7e98e5ac14d9189f77cff453965242db
Kay <joe_kayu@yahoo.com>
2020-09-05 19:17:10 +0100

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/popper.min.js';
+import 'popper.js/dist/umd/popper.min.js';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/popper.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
