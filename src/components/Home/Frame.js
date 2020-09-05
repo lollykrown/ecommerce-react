88adfca22af809e8047842eb34912bb3e5fc8edc
Kay <joe_kayu@yahoo.com>
2020-09-05 15:40:02 +0100

    .

--- src/components/Home/Frame.js
+++ src/components/Home/Frame.js
@@ -7,13 +7,12 @@ export default class Frame extends Component {
     console.log(this.props)
     return (
         <div className="container">
-            <Title name="" title="title"/>
+            <Title name="thhhh" title=""/>
            <div className="row">
               <div className="col-10 mx-auto text-center text-title
               text-uppercase pt-5">
                 <h1 className="display-3">4040</h1>
                 <h1>error</h1>
-                <h2>page not found</h2>
                 <h3> the requested URL<span className="text-danger">
                   </span>{" "} was not found</h3>
               </div>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import styled from "styled-components";
 
 export default function Title({name, title}){
     return (
-        <div className="row">
-            <div className="col-10 mx-auto my-2 text-center text-title">
-                <h1 className="text-capitalize font-weight-bold">
+        <TitleContainer className="row">
+            <div className="col-10 my-2 text-title">
+                <h1 className="text-capitalize textt">
                     {name} <strong className="text-blue">{title}</strong>
                 </h1>
             </div>
-        </div>
+        </TitleContainer>
     )
 }
+
+const TitleContainer = styled.div`
+    text-transform:capitalize;
+    font-size:1.125rem;
+`;
