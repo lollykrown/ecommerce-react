faa954327007aae3ad2657e73d4e15e15a5c9896
Kay <joe_kayu@yahoo.com>
2020-09-05 17:22:39 +0100

    .

--- src/components/Home/Item.js
+++ src/components/Home/Item.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Item({name}) {
+export default function Item({item}) {
     // const { id, title, img, price, total, count } = item;
     // const { increment, decrement, removeItem, handleDetail } = value
   return (
       <div className="row my-2 text-capitalize">
         <div className="">
         <Link to="/details">
-          <img className="img-fluid" src={'img'} />
-          <span className="">{name} </span>
+          <i className={item.icon}></i>
+          {/* <img className="img-fluid" src={'img'} /> */}
+          <span className="ml-3">{item.name} </span>
         </Link>
         </div>
       </div>
--- src/components/Home/ItemList.js
+++ src/components/Home/ItemList.js
@@ -3,23 +3,23 @@ import Item from "./Item";
 
 export default function ItemList({ value }) {
   const items = [
-    {id:1, name:'supermarket'},
-    {id:2, name:'health & beauty'},
-    {id:3, name:'home & office'},
-    {id:4, name:'phones & tablets'},
-    {id:5, name:'computing'},
-    {id:6, name:'electronics'},
-    {id:7, name:'fashion'},
-    {id:8,name:'baby products'},
-    {id:9, name:'gaming'},
-    {id:10,name:'sporting goods'},
-    {id:11,name:'automobile'},
-    {id:12,name:'other'}
+    {id:1, icon:'fas fa-shopping-basket',name:'supermarket'},
+    {id:2, icon:'fas fa-medkit',  name:'health & beauty'},
+    {id:3, icon:'fas fa-home', name:'home & office'},
+    {id:4, icon:'fas fa-mobile', name:'phones & tablets'},
+    {id:5, icon:'fas fa-laptop', name:'computing'},
+    {id:6, icon:'fas fa-tablet', name:'electronics'},
+    {id:7, icon:'fas fa-shopping-bag', name:'fashion'},
+    {id:8, icon:'fas fa-child', name:'baby products'},
+    {id:9, icon:'fas fa-gamepad', name:'gaming'},
+    {id:10,icon:'fas fa-futbol', name:'sporting goods'},
+    {id:11,icon:'fas fa-car', name:'automobile'},
+    {id:12,icon:'fas fa-dot-circle', name:'other'}
   ];
   return (
     <div className="pl-3 mt-1">
         {items.map(item => {
-            return <Item key={item.id} name={item.name}
+            return <Item key={item.id} item={item}
              />
         })}
     </div>
