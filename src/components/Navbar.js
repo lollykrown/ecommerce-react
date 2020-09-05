cd21008e60db8d5cdb9b883e3c6f756a97019995
Kay <joe_kayu@yahoo.com>
2020-09-05 08:26:40 +0100

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,45 +1,115 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
-import styled from 'styled-components';
-import {ButtonContainer} from './Button'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 class Navbar extends Component {
-  
-  render(){
+  render() {
     return (
       <NavWrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
-      {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
+        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
       Creative Commons (Attribution 3.0 Unported);
       https://www.iconfinder.com/Makoto_msk */}
         <Link to="/">
-          <img src={logo} alt="store" className="navbar-brand"/>
+          <img src={logo} alt="store" className="navbar-brand" />
         </Link>
-        <ul className="navbar-nav align-items-center">
-          <li className="nav-item ml-5">
-            <Link to="/" className="nav-link">
-              products
-            </Link>
-          </li>
-        </ul>
-        <Link to="/cart" className="ml-auto">
-          <ButtonContainer>
-            <span className="mr-2">
-            <i className="fas fa-cart-plus"></i>
-            </span>            
-            my cart</ButtonContainer>
-        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav align-items-center">
+            <li class="nav-item active ml-2">
+              <Link to="/" className="nav-link">
+                Home <span class="sr-only">(current)</span>
+              </Link>
+            </li>            
+            <li className="nav-item ml-5">
+              <Link to="/" className="nav-link">
+                products
+              </Link>
+            </li>
+            <li class="nav-item ml-2">
+              <Link to="/" className="nav-link">
+              Store Locator
+              </Link>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form class="form-inline ml-auto my-2 my-lg-0">
+            <input
+              class="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+              Search
+            </button>
+          </form>
+          <Link to="/cart" className="ml-auto">
+            <ButtonContainer>
+              <span >
+                <i className="fas fa-cart-plus mr-2"></i>
+              </span>
+              my cart
+              <span className="ml-2">0</span>
+            </ButtonContainer>
+          </Link>
+        </div>
       </NavWrapper>
     );
   }
 }
 
 const NavWrapper = styled.nav`
-    background:var(--mainBlue);
-    .nav-link{
-      color: var(--mainWhite);
-      font-size:1.3rem;
-      text-transform:capitalize;
-    }
+  background: var(--mainPurple) !important;
+  .nav-link {
+    color: var(--mainWhite);
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
 `;
 export default Navbar;
