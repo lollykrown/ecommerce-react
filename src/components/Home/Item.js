78f88ad180b97fa6aa7e019b78c7775e0cecd60d
Kay <joe_kayu@yahoo.com>
2020-09-06 13:42:50 +0100

    .

--- src/components/Home/Item.js
+++ src/components/Home/Item.js
@@ -2,16 +2,20 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Item({ item }) {
-  // const { id, title, img, price, total, count } = item;
-  // const { increment, decrement, removeItem, handleDetail } = value
-
-  // const [open, setOpen] = useState(false);
-  // const showDropdown = (e) => {
-  //   setOpen(!open);
-  // };
-  // const hideDropdown = (e) => {
-  //   setOpen(false);
-  // };
+  const items = [
+    {id:1, icon:'fas fa-shopping-basket',name:'supermarket'},
+    {id:2, icon:'fas fa-medkit',  name:'health & beauty'},
+    {id:3, icon:'fas fa-home', name:'home & office'},
+    {id:4, icon:'fas fa-mobile', name:'phones & tablets'},
+    {id:5, icon:'fas fa-laptop', name:'computing'},
+    {id:6, icon:'fas fa-tablet', name:'electronics'},
+    {id:7, icon:'fas fa-tshirt', name:'fashion'},
+    {id:8, icon:'fas fa-child', name:'baby products'},
+    {id:9, icon:'fas fa-gamepad', name:'gaming'},
+    {id:10,icon:'fas fa-futbol', name:'sporting goods'},
+    {id:11,icon:'fas fa-car', name:'automobile'},
+    {id:12,icon:'fas fa-dot-circle', name:'other'}
+  ];
   return (
     <div
       className="row my-1 py-1 text-capitalize dropdown"
