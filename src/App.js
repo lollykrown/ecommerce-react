205706bc8875b386ea78eb2088083f43f145b9de
lollykrown <joe_kayu@yahoo.com>
2020-10-26 17:27:37 +0100

    start

--- src/App.js
+++ src/App.js
@@ -5,38 +5,18 @@ import 'popper.js/dist/umd/popper.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
-import Navbar from './components/Navbar';
-import ProductList from './components/ProductList';
-import Details from './components/Details';
-import Cart from './components/Cart';
-import Default from './components/Default';
-import Modal from './components/Modal';
-import Home from './components/Home';
-import Footer from './components/Footer';
+import Header from './components/Header';
 
 
 function App(){
   
     return (
       <React.Fragment>
-        <Navbar/>
+        <Header/>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/accessories" component={() => <ProductList cat={"accessories"} />} />
-          <Route exact path="/health \& beauty" component={() => <ProductList cat={"beauty"} />}/>
-          <Route exact path="/watches" component={() => <ProductList cat={"jewelry"} />}/>
-          <Route exact path="/phones \& tablets" component={() => <ProductList cat={"phones"} /> } />
-          <Route exact path="/men" component={() => <ProductList cat={"men"} /> } />
-          <Route exact path="/women" component={() => <ProductList cat={"women"} /> } />
-          <Route exact path="/kids" component={() => <ProductList cat={"kids"} />} />
-          <Route exact path="/shoes" component={() => <ProductList cat={"shoes"} />} />
-          <Route exact path="/household items" component={() => <ProductList cat={"house"}/>} />
-          <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart} />
-          <Route component={Default} />
         </Switch>
         <Footer />
-        <Modal/>
       </React.Fragment>
     );
 }
