d49e0651f724afdc04cfb015660ff77c99974f1f
lollykrown <joe_kayu@yahoo.com>
2020-11-23 23:14:33 +0100

    .

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -43,11 +43,11 @@ exports.signin = (req, res) => {
             if (user) {
                 if (user.authenticate(req.body.password)) {
                     const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '1hr' });
-                    const { _id,firstName, lastName, email, role, fullName } = user
+                    const { _id, firstName, lastName, email, role, fullName } = user
 
                     res.status(200).json({
                         token,
-                        user: { _id,firstName, lastName, email, role, fullName }
+                        user: { _id, firstName, lastName, email, role, fullName }
                     })
                 } else {
                     return res.status(400).json({
@@ -59,32 +59,14 @@ exports.signin = (req, res) => {
                     message: 'something went wrong'
                 })
             }
-
-            const _user = new User({ firstName, lastName, email, password, username: Math.random().toString() })
-            _user.save((error, data) => {
-                if (error) {
-                    return res.status(400).json({
-                        message: 'something went wrong'
-                    })
-                }
-
-                if (data) {
-                    res.status(201).json({
-                        message: 'user created successfully',
-                        data
-                    })
-                }
-            })
         })
 
 }
 
 exports.requireSignin = (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.Authorization.split(' ')[1];
 
     const user = jwt.verify(token, process.env.JWT_SECRET)
     req.user = user
-    next()
-
-
+    next();
 }
